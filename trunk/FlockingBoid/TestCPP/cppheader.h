93aed2606a1aa078d6f61ed209e16a2e26a5f7f9
xiaoyaoth@gmail.com <xiaoyaoth@gmail.com@47fa4dc2-d32d-ae41-a9e3-f2e6b954d4b0>
2013-10-20 08:40:33 +0000

    CPP zcode phase 1 done     TODO:         phase 2: introducing the "shared memory"         GPU zcode

--- trunk/FlockingBoid/TestCPP/cppheader.h
+++ trunk/FlockingBoid/TestCPP/cppheader.h
@@ -7,16 +7,17 @@
 #include <iostream>
 
 //	Later to be specified in the XML file
-#define CELL_RESO	10
 #define BOARDER_L	0
 #define BOARDER_R	1000
 #define BOARDER_U	0
 #define BOARDER_D	1000
 #define AGENT_NO	1024
 #define BLOCK_SIZE	1024
-int XLENGTH = ((int)(BOARDER_R-BOARDER_L)/CELL_RESO);
-int CELL_NO = ((int)(BOARDER_R-BOARDER_L)/CELL_RESO)*
-	((int)(BOARDER_D-BOARDER_U)/CELL_RESO);
+#define DISCRETI	4
+int CNO_PER_DIM = (int)pow((float)2, DISCRETI);
+int CELL_NO = CNO_PER_DIM * CNO_PER_DIM;
+float CLEN_X = (float)(BOARDER_R-BOARDER_L)/CNO_PER_DIM;
+float CLEN_Y = (float)(BOARDER_D-BOARDER_U)/CNO_PER_DIM;
 
 typedef struct int_2d
 {
@@ -24,7 +25,24 @@ typedef struct int_2d
 	int y;
 	int_2d():x(0),y(0){}
 	int cell_id(){
-		return y * XLENGTH + x;
+		return y * CNO_PER_DIM + x;
+	}
+	int zCode(){
+		int xt = x;
+		int yt = y;
+		xt &= 0x0000ffff;                 // x = ---- ---- ---- ---- fedc ba98 7654 3210
+		xt = (xt ^ (xt << 8)) & 0x00ff00ff; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
+		xt = (xt ^ (xt << 4)) & 0x0f0f0f0f; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+		xt = (xt ^ (xt << 2)) & 0x33333333; // x = --fe --dc --ba --98 --76 --54 --32 --10
+		xt = (xt ^ (xt << 1)) & 0x55555555; // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+
+		yt &= 0x0000ffff;                  // x = ---- ---- ---- ---- fedc ba98 7654 3210
+		yt = (yt ^ (yt << 8)) & 0x00ff00ff; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
+		yt = (yt ^ (yt << 4)) & 0x0f0f0f0f; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+		yt = (yt ^ (yt << 2)) & 0x33333333; // x = --fe --dc --ba --98 --76 --54 --32 --10
+		yt = (yt ^ (yt << 1)) & 0x55555555; // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+
+		return xt | (yt << 1);
 	}
 	void print(){
 		printf("(%d, %d)", x, y);
