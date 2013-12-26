25399c64528d3ddb3979021f290414d8dcbb98a8
xiaoyaoth@gmail.com <xiaoyaoth@gmail.com@47fa4dc2-d32d-ae41-a9e3-f2e6b954d4b0>
2013-12-26 09:13:01 +0000

    Early shared memory

--- tags/early_shared_mem/FlockingBoid/gsimlib_header.cuh
+++ tags/early_shared_mem/FlockingBoid/gsimlib_header.cuh
@@ -17,6 +17,7 @@ __constant__ int BOARDER_D_D;	//copied from host
 __constant__ int CNO_PER_DIM;	//(int)pow((float)2, DISCRETI)
 __constant__ float CLEN_X;		//(float)(BOARDER_R-BOARDER_L)/CNO_PER_DIM;
 __constant__ float CLEN_Y;		//(float)(BOARDER_D-BOARDER_U)/CNO_PER_DIM;
+__constant__ float RANGE;		//read from config
 //__constant__ int SHARED_SCALE_D;//copied from host
 
 int CELL_NO;		//CNO_PER_DIM * CNO_PER_DIM;
@@ -28,6 +29,7 @@ int BOARDER_U_H;	//read from config
 int BOARDER_D_H;	//read from config
 int AGENT_NO;		//read from config
 int STEPS;			//read from config
+float RANGE_H;
 //int SHARED_SCALE_H;	//read from config
 int SELECTION;		//read from config
 bool VISUALIZE;		//read from config
